800f67e051f0d547fd3fd5d265caa3ebb79f25d0
Elmer Juventino Yam Ramirez <elmeryam5@gmail.com>
2020-07-30 11:29:07 -0500

    cambia el precioe de la venta por el precio del articulo

--- detail.php
+++ detail.php
@@ -11,7 +11,7 @@ $preference = new MercadoPago\Preference();
 
 // Crea un ítem en la preferencia
 $item = new MercadoPago\Item();
-$item->title = 'Mi producto';
+$item->title = "". $_POST['title'] .""  ;
 $item->quantity = 1;
 $item->unit_price = $_POST['price'];
 $preference->items = array($item);
