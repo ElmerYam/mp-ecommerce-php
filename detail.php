20a26e985b06832aed13711f59733ad00ae41ce9
Elmer Juventino Yam Ramirez <elmeryam5@gmail.com>
2020-07-29 20:30:07 -0500

    cambia el titulo de la venta por el nombre del articulo

--- detail.php
+++ detail.php
@@ -7,9 +7,11 @@ MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c749498
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
 
+
+
 // Crea un ítem en la preferencia
 $item = new MercadoPago\Item();
-$item->title = 'Mi producto';
+$item->title = $_POST['title'];
 $item->quantity = 1;
 $item->unit_price = 75.56;
 $preference->items = array($item);
